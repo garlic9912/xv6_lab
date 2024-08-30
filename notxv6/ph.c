e1e0a0e066977af7347f3afd5ddef0b020a6b192
garlic9912 <2246257278@qq.com>
2024-08-30 19:12:27 +0800

    lab7

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,9 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+// 声明锁
+pthread_mutex_t lock[NBUCKET];
+
 double
 now()
 {
@@ -27,7 +30,7 @@ now()
 
 static void 
 insert(int key, int value, struct entry **p, struct entry *n)
-{
+{   
   struct entry *e = malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
@@ -51,7 +54,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);    
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]); 
   }
 }
 
@@ -102,6 +107,12 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+
+  // 初始化锁
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&lock[i], NULL);
+  }
+
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
